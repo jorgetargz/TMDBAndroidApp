4c078435abe42a7023373a31109193039a144b08
Jorge Martín Llorente <jorgemartin99@outlook.com>
2023-01-19 12:13:48 +0100

    navigation args const val for keys

--- app/src/main/java/org/jorgetargz/movies/framework/common/Constantes.kt
+++ app/src/main/java/org/jorgetargz/movies/framework/common/Constantes.kt
@@ -1,6 +1,9 @@
 package org.jorgetargz.movies.framework.common
 
 object Constantes {
+    const val ID_MOVIE = "idMovie"
+    const val ID_TV_SHOW = "idTVShow"
+    const val ID_PERSON = "idPerson"
     const val GITHUB_PROFILE_URL = "https://github.com/jorgetargz"
     const val SPLASH_IMAGE = "https://play-lh.googleusercontent.com/_nANpYA2cXZv0Ll0Ibyv2FZXoYGrwbuwR0WCa-VfQ76-TsUW5XWRACIZnjqYPc4mKpg=h500"
 }
