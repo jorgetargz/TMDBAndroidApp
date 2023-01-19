314cde136b65eb669d9affb7e04559ec8c17d7a0
Jorge Martín Llorente <jorgemartin99@outlook.com>
2023-01-19 12:20:44 +0100

    supportNavigateUp appBar back button

--- app/src/main/java/org/jorgetargz/movies/framework/main/MainActivity.kt
+++ app/src/main/java/org/jorgetargz/movies/framework/main/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -107,4 +108,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
